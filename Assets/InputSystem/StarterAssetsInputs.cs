373b67fd216b0f59563219f1a67cc5256200cfcb
Joel <120654417+JoelDesigner3D@users.noreply.github.com>
2023-07-04 08:28:44 +0200

    Mouvement caméra avec click

--- Assets/InputSystem/StarterAssetsInputs.cs
+++ Assets/InputSystem/StarterAssetsInputs.cs
@@ -18,17 +18,38 @@ namespace StarterAssets
 
 		[Header("Mouse Cursor Settings")]
 		public bool cursorLocked = true;
-		public bool cursorInputForLook = true;
+		public bool cursorInputForLook = false;
+
+		private Mouse mouse;
+
+        private void Awake()
+        {
+			mouse = Mouse.current;
+        }
+		
+        private void Update()
+        {
+            if (mouse.leftButton.isPressed)
+            {
+				cursorInputForLook = true;
+            }
+            else
+            {
+				cursorInputForLook = false;
+				LookInput(new Vector2(0f, 0f));
+			}
+        }
+
 
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
-		public void OnMove(InputValue value)
+        public void OnMove(InputValue value)
 		{
 			MoveInput(value.Get<Vector2>());
 		}
-
+		
 		public void OnLook(InputValue value)
 		{
-			if(cursorInputForLook)
+			if(cursorInputForLook && mouse.leftButton.isPressed)
 			{
 				LookInput(value.Get<Vector2>());
 			}
