16dbfc2a0a7836344701e1a900bb0cfe9f1c54e6
Joel <120654417+JoelDesigner3D@users.noreply.github.com>
2023-07-06 12:13:22 +0200

    Nutriscore

--- Assets/Scripts/FoodPanel.cs
+++ Assets/Scripts/FoodPanel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using ColorUtility = UnityEngine.ColorUtility;
 
 public class FoodPanel : MonoBehaviour
 {
@@ -13,6 +14,9 @@ public class FoodPanel : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _carbohidrates;
     [SerializeField] private TextMeshProUGUI _lipids;
     [SerializeField] private TextMeshProUGUI _nutriscore;
+
+    private string[] nutriScoreColors = new string[]{ "#038141", "#85bb2f", "#fecb02", "#ee8100", "#e63e11" };
+    private string[] nutriScoreIndices = new string[]{"A", "B", "C", "D", "E"};
 
     private void Update()
     {
@@ -26,33 +30,59 @@ public class FoodPanel : MonoBehaviour
     {
         _name.text = text;
     }
-    public void SetCalories(string text)
+    public void SetCalories(float value)
     {
-        _calories.text = text;
+        _calories.text = "Calories : " + value;
     }
-    public void SetProteins(string text)
+    public void SetProteins(float value)
     {
-        _proteins.text = text;
+        _proteins.text = "Protéines : " + value;
     }
-    public void SetLipids(string text)
+    public void SetLipids(float value)
     {
-        _lipids.text = text;
+        _lipids.text = "Lipides : " + value;
     }
-    public void SetCarbohidrates(string text)
+    public void SetCarbohidrates(float value)
     {
-        _carbohidrates.text = text;
+        _carbohidrates.text = "Protides : " + value;
     }
-    public void SetNutriScore(string text)
+
+    // Values [A,B,C,D,E] = [2, 1, 0, -1, -2]
+    public void SetNutriScore(int score)
     {
-        _nutriscore.text = text;
+        if (score <-2 || score > 2)
+        {
+            _nutriscore.text = "NutriScore : ";
+            return;
+        }
+
+        score += 2;
+
+        Debug.Log(score);
+
+        string htmlColor = nutriScoreColors[4 - score];
+
+        Color rgbColor;
+        if (ColorUtility.TryParseHtmlString(htmlColor, out rgbColor))
+        {
+            Debug.Log("Couleur RVB : " + rgbColor);
+            _nutriscore.color = rgbColor;
+        }
+        else
+        {
+            Debug.LogError("Erreur de conversion de couleur HTML en RVB");
+        }
+        
+        _nutriscore.text = "NutriScore : " + nutriScoreIndices[4-score];
     }
+
     public void SetProperties(FoodProperties _foodProperties)
     {
         this.SetName(_foodProperties.Fr);
-        this.SetCalories("Calories : "+_foodProperties.Calories);
-        this.SetProteins("Protéines : " + _foodProperties.Proteins);
-        this.SetCarbohidrates ("Protides : " + _foodProperties.Carbohydrates);
-        this.SetLipids("Lipides : " + _foodProperties.Lipids);
-        this.SetNutriScore("NutriScore : " + (_foodProperties.Nutriscore + 2));
+        this.SetCalories(_foodProperties.Calories);
+        this.SetProteins(_foodProperties.Proteins);
+        this.SetCarbohidrates (_foodProperties.Carbohydrates);
+        this.SetLipids(_foodProperties.Lipids);
+        this.SetNutriScore(_foodProperties.Nutriscore);
     }
 }
